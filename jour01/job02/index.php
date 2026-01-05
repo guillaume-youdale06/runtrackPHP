22df94cbce1dfba70ea81d8179957c270406e90a
guillaume-youdale <guillaume.youdale@laplateforme.io>
2026-01-05 13:04:38 +0100

    Job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Hello LaPlateforme";     //Affiche la phrase (façon basique)
+echo "<br>";
+
+print "Hello LaPlateforme";     #Affiche la phrase (similaire à écho)
+echo "<br>";
+
+$phrase = "Hello LaPlateforme"; 
+var_dump($phrase);              /*Affiche la phrase ainsi que son type (str)*/
+
+?>

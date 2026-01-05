80b9e648b76432c5933d71664222f033f98bd326
guillaume-youdale <guillaume.youdale@laplateforme.io>
2026-01-05 12:50:38 +0100

    Job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$str = "LaPlateforme";
+echo($str . "<br>");
+
+$str2 = "Vive";
+$str3 = "!";
+echo($str2 . " " . $str . " " . $str3 . "<br>");
+
+$val = 6;
+echo($val);
+echo " ";
+$val += 4;
+echo($val);
+echo "<br>";
+
+$myBool = true;
+echo($myBool . " "); #true : devient le chiffre 1
+var_dump($myBool);
+echo "<br>";
+$myBool = false;
+echo($myBool); #false : Chaîne de caractère vide
+var_dump($myBool);
+
+?>

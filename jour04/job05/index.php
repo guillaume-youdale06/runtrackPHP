cf0ef7f1a9c22e83d5dd4cfbc8bb72d00f0d774d
guillaume-youdale <guillaume.youdale@laplateforme.io>
2026-01-09 11:33:24 +0100

    Job05

---
+++ jour04/job05/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job04</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="nom">Username :</label>
+        <input type="text" name="username" placeholder="pseudo">
+        <label for="mdp">Mot de passe :</label>
+        <input type="password" name="mdp">
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <?php
+        if (!empty($_POST)) {
+            if ($_POST["username"] == "John" && $_POST["mdp"] == "Rambo") {
+                echo "c'est pas ma guerre";
+            } else {
+                echo "votre pire cauchemar";
+            }
+        }
+    ?>
+    
+</body>
+</html>

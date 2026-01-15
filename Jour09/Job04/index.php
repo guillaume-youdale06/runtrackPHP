387d17dbf448b1e596ea47d86eb7613b2dfc8a16
guillaume-youdale <guillaume.youdale@laplateforme.io>
2026-01-15 12:11:02 +0100

    Job04

---
+++ Jour09/Job04/index.php
@@ -0,0 +1,49 @@
+<?php
+
+try {
+   $pdo = new PDO("mysql:host=localhost;dbname=Jour08;charset=utf8", "root", "");
+   $sql = "SELECT * FROM etudiants WHERE prenom LIKE 'T%'";
+   $req = $pdo -> prepare($sql);
+   $req -> setFetchMode(PDO::FETCH_ASSOC);
+   $req -> execute();
+
+} catch(PDOException $e) {
+   $pdo -> rollBack();
+   echo "Erreur : " . $e->getMessage();
+}
+
+$tab = $req -> fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Job05</title>
+</head>
+<body>
+   <table border=1>
+
+      <?php
+
+         echo "<thead><tr>";
+         foreach(array_keys($tab[0]) as $colName) {
+            echo "<th>".$colName."</th>";
+         }
+         echo "</tr><thead>";
+
+         foreach($tab as $lst) {
+            echo "<tr>";
+            foreach($lst as $val) {
+               echo "<td>".$val."</td>";
+            }
+            echo "</tr>";
+         }
+
+      ?>
+
+   </table>  
+</body>
+</html>
